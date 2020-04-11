611d50c226a46bde514507c8ab7deb150f9e7f22
Rottenhoffer Olivér <orottenhoffer@geomant.com>
2020-04-11 15:01:04 +0200

    replace auto generated fragments

--- app/src/main/java/hu/bme/aut/bgg_thehotness/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/hu/bme/aut/bgg_thehotness/ui/favorites/FavoritesFragment.kt
@@ -1,9 +1,26 @@
 package hu.bme.aut.bgg_thehotness.ui.favorites
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import hu.bme.aut.bgg_thehotness.R
 import hu.bme.aut.bgg_thehotness.model.BoardGame
 
 class FavoritesFragment : Fragment(), FavoritesScreen {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val textView: TextView = root.findViewById(R.id.text_favorites)
+        textView.text = "Favorites fragment";
+        return root
+    }
 
     override fun showFavoriteBoardGames(favoriteGames: List<BoardGame>) {
         TODO("Not yet implemented")
--- app/src/main/java/hu/bme/aut/bgg_thehotness/ui/hotness/HotnessFragment.kt
+++ app/src/main/java/hu/bme/aut/bgg_thehotness/ui/hotness/HotnessFragment.kt
@@ -1,9 +1,26 @@
 package hu.bme.aut.bgg_thehotness.ui.hotness
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import hu.bme.aut.bgg_thehotness.R
 import hu.bme.aut.bgg_thehotness.model.BoardGameWithIsFavorite
 
 class HotnessFragment : Fragment(), HotnessScreen {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_hotness, container, false)
+        val textView: TextView = root.findViewById(R.id.text_hotness)
+        textView.text = "The hotness fragment";
+        return root
+    }
 
     override fun showHotBoardGames(hotGames: List<BoardGameWithIsFavorite>) {
         TODO("Not yet implemented")
